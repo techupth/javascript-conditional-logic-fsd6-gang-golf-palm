d64d995352e6b3bf33e4b6ca1a24417d909cc391
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-15 14:22:21 +0700

    feat: solve exercise 1-7

--- ex-4.js
+++ ex-4.js
@@ -2,4 +2,12 @@
 // Switch Statement
 let lightBulbStatus = "On";
 
-//Start coding here
+switch (lightBulbStatus){
+    case "On":
+        console.log("Light bulb is On");
+        break;
+    case "Broken":
+        console.log("Light bulb is Broken");
+    default:
+        console.log("Please choose the correct input (On/Off)")     ;
+} 
