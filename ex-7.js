6155be57ed58bbb9314ba5a65a4c177de4734161
fossi13 <fossi13@users.noreply.github.com>
2024-05-15 13:24:47 +0700

    finish ex1-ex6

--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,9 @@
 //Start coding here
-const greeting = "Hello ! 😁";
 
-greeting();
+const greeting = () => {
+  return "Hello ! 😁";
+};
+
+console.log(greeting());
 
 console.log("This line will be executed without any error.");
